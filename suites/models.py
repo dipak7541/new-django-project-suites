b526e42fd975f515614a073307e3d0c617b99685
Tanka <tiwaritanka75@gmail.com>
2020-03-05 12:27:44 +0545

    adding models to show people's commments into the website

--- suites/models.py
+++ suites/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class PeopleSay(models.Model):
+    img = models.ImageField()
+    name = models.CharField(max_length=20)
+    comments= models.TextField()
+    
+
 # Create your models here.
