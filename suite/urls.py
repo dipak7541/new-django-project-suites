18051a2b9e2213da3fd9ecedb5152a892dfaa9cb
Tanka <tiwaritanka75@gmail.com>
2020-03-07 14:54:19 +0545

    created models for databse to store data which generated while customer send messages through contact page

--- suite/urls.py
+++ suite/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('suite', include('suites.urls')),
+    path('', include('suites.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- suites/models.py
+++ suites/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 
 class ManxekoBichar(models.Model):
     name = models.CharField(max_length=20)
-    comments= models.CharField(max_length=200)
-    date= models.DateField()
+    comments = models.CharField(max_length=200)
+    date = models.DateField()
 
+class ContactDetails(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phoneno = models.CharField(max_length=12)
+    message = models.TextField()
+    date = models.DateTimeField(default=now, editable=False)
 
 
 # Create your models here.
